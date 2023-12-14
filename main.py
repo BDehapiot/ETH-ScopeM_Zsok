b449038f32002601e64f433282095d2c20d62a31
bdehapiot <b.dehapiot@gmail.com>
2023-12-14 16:25:53 +0100

    update

--- main.py
+++ main.py
@@ -2,110 +2,152 @@
 
 import nd2
 import cv2
+import csv
+import time
 import numpy as np
-from skimage import io 
 from pathlib import Path
-from joblib import Parallel, delayed 
+import matplotlib.pyplot as plt
+from joblib import Parallel, delayed
+
+# Skimage
+from skimage.filters import gaussian
+from skimage.feature import peak_local_max
+from skimage.morphology import disk, binary_dilation
 
 #%% Parameters ----------------------------------------------------------------
 
 sigma = 1
-radius = 5
-rsize_factor = 0.1
-minProm = 20
-minDist = 3
+minProm = 6
+minDist = 4
+roiRadius = 4 
 
 #%% Initialize ----------------------------------------------------------------
 
-data_path = 'D:\local_Zsok\data'
-# stack_name = 'WellG01_PointG01_0000_ChannelSD GFP_Seq0000.nd2' # Mlp1 0hr
-stack_name = 'WellO13_PointO13_0000_ChannelSD GFP_Seq0000.nd2' # Mlp1 7hr
-# stack_name = 'WellF20_PointF20_0000_ChannelSD GFP_Seq0000.nd2' # Mlp1 14hr
+data_path = Path('D:\local_Zsok\data')  
+conds = ["eGFP", "Mlp1", "Mlp2", "Nup96", "Nup133", "Pml39", "dpml39"]
+nProt = [     1,     16,      8,      32,       16,       4,        8]
 
-stack = nd2.imread(Path(data_path) / stack_name) 
-stack = stack[10:20,500:1500,500:1500]
-
+# Get stack names
+stack_names = []
+for stack_path in data_path.iterdir():
+    if stack_path.is_file():
+        stack_names.append(stack_path.name)
+        
 #%% Process -------------------------------------------------------------------
 
-from skimage.filters import gaussian
-from skimage.restoration import rolling_ball
-from skimage.feature import peak_local_max
-from skimage.morphology import disk, white_tophat, binary_dilation, dilation, ball
-from skimage.transform import downscale_local_mean, resize
-from skimage.segmentation import expand_labels
-from skimage.measure import label
+def process(stack_name):
 
-def process(
-        stack, 
-        sigma=1, 
-        radius=5, 
-        rsize_factor=0.1, 
-        minProm=10,
-        minDist=3, 
-        ):
+    # Open stack & info
+    stack = nd2.imread(Path(data_path) / stack_name) 
+    date, cond, numb = stack_name.replace(".nd2", "").split("_")    
+       
+    # Standard projection (stdProj)
+    stdProj = np.std(stack, axis=0)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+    opened = cv2.morphologyEx(stdProj, cv2.MORPH_OPEN, kernel)
+    stdProj = cv2.subtract(stdProj, opened, dst=stdProj)
+    stdProj = gaussian(stdProj, sigma)
+    
+    # Sum projection (sumProj)
+    sumProj = np.sum(stack, axis=0).astype('float32')
+    
+    # Local max detection
+    coords = peak_local_max(
+        stdProj, min_distance=minDist, threshold_abs=minProm
+        ).astype(int)
+    
+    # Background measurment
+    bgROI = np.zeros_like(sumProj, dtype=bool)
+    bgROI[coords[:,0], coords[:,1]] = True
+    bgROI = binary_dilation(bgROI, footprint=disk(roiRadius * 1.25)) 
+    bgROI ^= binary_dilation(bgROI, footprint=disk(roiRadius * 1))
+    bg = sumProj.copy()
+    bg[bgROI!=True] = np.nan
+    bg = np.nanmean(bg)
+    
+    # Local max measurments
+    dot_data = []
+    strel = disk(roiRadius, dtype=float)
+    strel[strel==0] = np.nan
+    for coord in coords:
+        y = coord[0]; x = coord[1]
+        if (y - roiRadius >= 20 and y + roiRadius <= sumProj.shape[0] -20 and 
+            x - roiRadius >= 20 and x + roiRadius <= sumProj.shape[1] -20):       
+    
+            ROI = sumProj[
+                y - roiRadius:y + roiRadius + 1,
+                x - roiRadius:x + roiRadius + 1
+                ] * strel
+                    
+            dot_data.append((stack_name, date, cond, numb, x, y, np.nanmean(ROI) - bg))
+            
+    stack_data = (stack_name, date, cond, numb, stdProj, sumProj, dot_data)
+    
+    return stack_data
 
-    # Scale parameters acc. to rsize_factor
-    radius = round(radius * rsize_factor)
-    
-    def _process(img):
-        
-        # preprocess image
-        img = gaussian(img, sigma, preserve_range=True)
-        rscale_img = resize(
-            img, (img.shape[0]*rsize_factor, img.shape[1]*rsize_factor))
-        background = rolling_ball(rscale_img, radius=radius)
-        background = resize(background, (img.shape[0], img.shape[1]))
-        img = img - background
-        
-        # Local max detection
-        coords = peak_local_max(
-            img, min_distance=minDist, threshold_abs=minProm
-            ).astype(int)
-        
-        # Local max display
-        display = np.zeros(img.shape, dtype=bool)
-        display[coords[:,0], coords[:,1]] = True
-        display = label(display)
-        display = expand_labels(display, distance=5)
-        
-        return img, coords, display
-    
-    # Parallel processing
-    outputs = Parallel(n_jobs=-1)(
-        delayed(_process)(img) 
-        for img in stack
-        )
-    
-    # Extract outputs
-    pStack = np.stack([data[0] for data in outputs])
-    display = np.stack([data[2] for data in outputs])
-    coords = [data[1] for data in outputs]
-    
-    return pStack, coords, display
+start = time.time()
+print('Process')
 
-# Process raw stack image
-pStack, coords, display = process(
-    stack, 
-    sigma=sigma, 
-    radius=radius, 
-    rsize_factor=rsize_factor, 
-    minDist=minDist, 
-    minProm=minProm,
+# Parallel processing
+outputs = Parallel(n_jobs=-1)(
+    delayed(process)(
+        stack_name,
+        ) 
+    for stack_name in stack_names
     )
 
-#%% ctrd -------------------------------------------------------------------
+stack_data = [data for data in outputs]
+dot_data = [dot_data for data in outputs for dot_data in data[6]]
 
-# import napari
-# viewer = napari.Viewer()
-# viewer.add_image(pStack)
-# viewer.add_labels(display)
+end = time.time()
+print(f'  {(end-start):5.3f} s')
 
-# viewer.add_image(img)
-# points_layer = viewer.add_points(
-#     coords, 
-#     size=20,
-#     edge_width=0.1,
-#     edge_color='red',
-#     face_color='transparent',
-#     opacity = 0.5,
-#     )
+#%% Results -------------------------------------------------------------------
+
+# Plot results
+fig, ax = plt.subplots(len(conds), 1, figsize=(6, 2*len(conds)))
+for i, cond in enumerate(conds):
+    intDen = [data[6] for data in dot_data if data[2] == cond]
+    ax[i].hist(intDen, bins=100)
+    ax[i].set_xlim((-250, 1500))
+    ax[i].text(0.98, 0.94, f'{conds[i]} ({nProt[i]})\n{np.mean(intDen):.2f}', 
+                fontsize=12, ha='right', va='top', transform=ax[i].transAxes)
+    
+#%% Displays ------------------------------------------------------------------
+
+showCond = "Nup96"
+
+# Extract processed images & info
+names = [data[0] for data in stack_data if data[2] == showCond]
+stdProj = np.stack([data[4] for data in stack_data if data[2] == showCond])
+sumProj = np.stack([data[5] for data in stack_data if data[2] == showCond])
+x = [dot_data[4] for data in stack_data if data[2] == showCond for dot_data in data[6]]
+y = [dot_data[5] for data in stack_data if data[2] == showCond for dot_data in data[6]]
+i = [i for i, data in enumerate(stack_data) if data[2] == showCond for dot_data in data[6]]
+mapping = {value: i for i, value in enumerate(sorted(set(i)))}
+i = [mapping[i] for i in i]
+coords = nd_array = np.column_stack((i, y, x))
+
+# Display results in napari
+import napari
+viewer = napari.Viewer()
+viewer.add_image(sumProj, visible=True, contrast_limits=[3000, 9000])
+viewer.add_image(stdProj, visible=False, contrast_limits=[0, 30])
+points_layer = viewer.add_points(
+    coords, 
+    size=roiRadius*2,
+    edge_width=0.1,
+    edge_color='red',
+    face_color='transparent',
+    opacity = 0.5,
+    )
+
+# Show current image info
+current_slice = viewer.dims.current_step[0]
+text_labels = [f'{name}' for name in names]
+viewer.text_overlay.text = text_labels[current_slice]
+viewer.text_overlay.visible = True
+def update_text_label(event):
+    current_slice = viewer.dims.current_step[0]
+    viewer.text_overlay.text = text_labels[current_slice]
+viewer.dims.events.current_step.connect(update_text_label)
